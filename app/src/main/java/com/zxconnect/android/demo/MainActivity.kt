5e4158044828b1ffb421f95d68869e29392e49a9
Eric Ampire <ericampire.top@gmail.com>
2020-03-11 13:19:20 +0200

    Chat interface

--- app/src/main/java/com/zxconnect/android/demo/MainActivity.kt
+++ app/src/main/java/com/zxconnect/android/demo/MainActivity.kt
@@ -2,11 +2,66 @@ package com.zxconnect.android.demo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.databinding.BindableItem
+import com.xwray.groupie.kotlinandroidextensions.GroupieViewHolder
+import com.zxconnect.android.demo.databinding.ItemMessageReceiveBinding
+import com.zxconnect.android.demo.databinding.ItemMessageSendBinding
+import com.zxconnect.android.demo.model.Message
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private val messageAdapter = GroupAdapter<GroupieViewHolder>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView.adapter = messageAdapter
+
+        button.setOnClickListener {
+            val message = Message(text = editText.text.toString(), sendBy = "me")
+            val sendMessageItem = SendMessageItem(message)
+
+            messageAdapter.add(sendMessageItem)
+            editText.text.clear()
+
+            receiveAutoResponse()
+        }
+    }
+
+    private fun receiveAutoResponse() {
+        GlobalScope.launch(Dispatchers.Main) {
+            delay(1000)
+            val receive = Message(text = "Salut l'amis j'espere que vous allez bien, je suis tres bien j'ai manger to day", sendBy = "me")
+            val receiveItem = ReceiveMessageItem(receive)
+
+            messageAdapter.add(receiveItem)
+        }
+    }
+}
+
+class SendMessageItem(private val message: Message) : BindableItem<ItemMessageSendBinding>() {
+    override fun getLayout(): Int {
+        return R.layout.item_message_send
+    }
+
+    override fun bind(viewBinding: ItemMessageSendBinding, position: Int) {
+        viewBinding.message = message
+    }
+}
+
+class ReceiveMessageItem(private val message: Message) : BindableItem<ItemMessageReceiveBinding>() {
+    override fun getLayout(): Int {
+        return R.layout.item_message_receive
+    }
+
+    override fun bind(viewBinding: ItemMessageReceiveBinding, position: Int) {
+        viewBinding.message = message
     }
 }
---
+++ app/src/main/java/com/zxconnect/android/demo/model/Message.kt
@@ -0,0 +1,6 @@
+package com.zxconnect.android.demo.model
+
+class Message(
+    val text: String = "",
+    val sendBy: String = ""
+)
